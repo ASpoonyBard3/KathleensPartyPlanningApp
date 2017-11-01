b65eff83164c0db5ca1323903f53478ebd5d9ea6
ASpoonyBard3 <Samsonf21@gmail.com>
2017-11-01 19:01:17 +0000

    KathleensPartyPlanningApp

---
+++ KathleensPartyPlanningApp/DinnerParties.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KathleensPartyPlanningApp
+{
+    class DinnerParties
+    {
+        public void int NumberOfPeople;
+
+        public decimal CostOfDecorations;
+
+        public const void int CostOfFoodPerPerson = 25;
+
+
+        public CalculateCostOfDecorations()
+        {
+
+        }
+
+        public  SetHealthOption()
+        {
+
+        }
+
+        public CalculateCost()
+        {
+
+        }
+
+    }
+}
